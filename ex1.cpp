408ff44f3bdbd612bd5b96245e49ac6ba1295a47
JOÃO CARVALHO <dev.joaocarvalho@gmail.com>
2025-10-21 12:14:53 -0300

    Update ex1.cpp

--- ex1.cpp
+++ ex1.cpp
@@ -1,6 +1,6 @@
 /*-------------------------------------------------------------------------------------------------------------------------
-Autora    : Maria Clara Fontenele Silva
-Matrícula : 2312130230
+Autores    : Maria Clara Fontenele Silva E João Marcos Santos E Carvalho
+Matrícula : 2312130230                      231112130063
 Exercício 1 - Imprimir "Hello World!" na tela e exemplificar atomic/critical.
 --------------------------------------------------------------------------------------------------------------------------
 a) Crie uma região paralela com #pragma omp parallel.
@@ -21,33 +21,33 @@ int contador_compartilhado = 0;
 //Cores e Estilo
 namespace ConsoleColors {
     const std::string RESET = "\033[0m";      // Desfaz todas as formatações
-    const std::string VERMELHO = "\033[31m";  // Texto Vermelho
     const std::string VERDE = "\033[32m";     // Texto Verde
     const std::string NEGRITO = "\033[1m";    // Negrito/Brilho
 }
 
 int main() {
-    // Define a diretiva OpenMP para criar uma região paralela.
-    #pragma omp parallel num_threads(4)
-    {
-        // Obtém o número da thread atual
-        int thread_id = omp_get_thread_num();
-        // Obtém o número total de threads que estão executando ativamente a região paralela atual
-        int total_threads = omp_get_num_threads();
+    //Define a diretiva OpenMP para criar uma região paralela com 4 threads
+    #pragma omp parallel num_threads(4)
+    {
+        //Vaiável para armazenar o ID da thread atual
+        int thread_id = omp_get_thread_num();
+        //Vaiável para armazenar o número total de threads
+        int total_threads = omp_get_num_threads();
 
-        // Exemplo de 'atomic': Operação atômica em uma única variável compartilhada
-        #pragma omp atomic
-        contador_compartilhado++; // Garante que o incremento seja feito de forma segura.
+        //EXEMPLO DE ATOMIC -> operação atômica em uma única variável compartilhada
+        #pragma omp atomic
+        contador_compartilhado++; // Incrementa o contador compartilhado de forma atômica
 
-        // Exemplo de 'critical': Usado para proteger blocos maiores ou chamadas complexas (como E/S)
-        #pragma omp critical // garante que apenas uma thread por vez possa executar o bloco a seguir
-        // Imprime a mensagem "Hello world from thread " com o número da thread e o total de threads
-        std::cout << "Hello world from thread: " << thread_id << " of " << total_threads << std::endl;
-
-    }
-    std::cout << ConsoleColors::NEGRITO << ConsoleColors::VERDE
-              << "\nResultado final do contador após a regão paralela"
-              << ConsoleColors::RESET << "\n" << "\n";
-    std::cout << "\nValor final do contador compartilhado (via atomic): " << contador_compartilhado << std::endl;
-    return 0;
+        //EXEMPLO DE CRITICAL -> seção crítica para proteger o bloco de código
+        #pragma omp critical  // garante que apenas uma thread por vez possa executar o bloco a seguir
+        std::cout << "Hello world from thread: " << thread_id << " of " << total_threads << std::endl;
+    }
+        std::cout << ConsoleColors::NEGRITO << ConsoleColors::VERDE
+              << "\nValor final do contador compartilhado (via atomic): " << contador_compartilhado << std::endl << "\n"
+              << ConsoleColors::RESET << "\n";
+        return 0;
 }
+
+// Explicação:
+// A diretiva 'atomic' é usada para garantir que a operação de incremento do contador_comp  artilhado seja realizada de forma atômica,
+// ou seja, sem interferência de outras threads. Isso é crucial para evitar condições de corrida
